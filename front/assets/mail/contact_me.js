03faefa0f80e064f716e6710bbcb2a6d24279ac5
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2020-10-21 21:28:24 -0300

    atualizações no SouSandra, contato e proposta

--- front/assets/mail/contact_me.js
+++ front/assets/mail/contact_me.js
@@ -39,7 +39,7 @@ $(function () {
                         )
                         .append("</button>");
                     $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
+                        "<strong>Obrigado por nos dar sua opnião!</strong>"
                     );
                     $("#success > .alert-success").append("</div>");
                     //clear all fields
@@ -47,20 +47,18 @@ $(function () {
                 },
                 error: function () {
                     // Fail message
-                    $("#success").html("<div class='alert alert-danger'>");
-                    $("#success > .alert-danger")
+                    $("#success").html("<div class='alert alert-success'>");
+                    $("#success > .alert-success")
                         .html(
                             "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
                         )
                         .append("</button>");
-                    $("#success > .alert-danger").append(
+                    $("#success > .alert-success").append(
                         $("<strong>").text(
-                            "Sorry " +
-                                firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
-                        )
+                            "Obrigado por nos dar sua opnião " +
+                                firstName)
                     );
-                    $("#success > .alert-danger").append("</div>");
+                    $("#success > .alert-success").append("</div>");
                     //clear all fields
                     $("#contactForm").trigger("reset");
                 },
